3fe65a050d577e7f8738cdc2529100f45a9b9f94
Çetin Furkan Çakın <88437562+Cetin-Furkan@users.noreply.github.com>
2026-07-24 00:00:33 +0300

    Add Wayland initialization header file

---
+++ include/init/wayland_init.h
@@ -0,0 +1,9 @@
+#ifndef WAYLAND_INIT_H
+#define WAYLAND_INIT_H
+
+#include "graph_core/wayland_core.h"
+#include "graph_core/vulkan_core.h"
+
+bool wayland_init(struct Application *app);
+
+#endif // WAYLAND_INIT_H
